ea406a884cbcc37cf66b6c946101cc561669cc01
Yana <yanaponom2006@gmail.com>
2025-11-18 12:05:04 +0200

    Interaction between components

--- src/app/shared/components/item-card/item-card.ts
+++ src/app/shared/components/item-card/item-card.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Trip } from '../../models/trip.model';
 
 @Component({
   selector: 'app-item-card',
   standalone: true,
-  imports: [CommonModule],  
+  imports: [CommonModule],
   templateUrl: './item-card.html',
   styleUrls: ['./item-card.css']
 })
 export class ItemCard {
   @Input() trip!: Trip;
+
+  @Output() selectTrip = new EventEmitter<Trip>();
+
+  onSelect() {
+    this.selectTrip.emit(this.trip);
+  }
 }
