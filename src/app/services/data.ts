bdf75a957f336ff9d04bdc312f53ab2061f4548e
Yana <yanaponom2006@gmail.com>
2025-11-24 20:18:05 +0200

    Logic translation in the service

---
+++ src/app/services/data.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Trip } from '../shared/models/trip.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripDataService {
+
+  private trips: Trip[] = [
+    {
+      id: 1,
+      name: 'Венеційська пригода',
+      destination: 'Венеція',
+      startDate: '2025-12-01',
+      endDate: '2025-12-07',
+      price: 1200,
+      description: 'Подорож каналами Венеції.',
+      imageUrl: 'https://andy-travel.com.ua/sites/default/files/venice_grand_chanel_22.jpg',
+      isSpecial: true
+    },
+    {
+      id: 2,
+      name: 'Гірський трекінг',
+      destination: 'Альпи',
+      startDate: '2026-01-10',
+      endDate: '2026-01-20',
+      price: 1500,
+      description: 'Активний відпочинок серед гір.',
+      imageUrl: 'https://gra.travel/media/images/first-alps.width-1920.jpg',
+      isSpecial: false
+    }
+  ];
+
+  getItems(): Trip[] {
+    return this.trips;
+  }
+}
--- src/app/shared/components/items-list/items-list.ts
+++ src/app/shared/components/items-list/items-list.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ItemCard } from '../item-card/item-card';
+import { TripDataService } from '../../../services/data';
 import { Trip } from '../../models/trip.model';
+import { ItemCard } from '../item-card/item-card'; // приклад імпорту дочірнього компонента, якщо є
 
 @Component({
   selector: 'app-items-list',
@@ -11,17 +12,18 @@ import { Trip } from '../../models/trip.model';
   templateUrl: './items-list.html',
   styleUrls: ['./items-list.css']
 })
-export class ItemsList {
-  trips: Trip[] = [
-    { id: 1, name: 'Венеційська пригода', destination: 'Венеція', startDate: '2025-12-01', endDate: '2025-12-07', price: 1200, description: 'Подорож каналами Венеції.', imageUrl: 'https://andy-travel.com.ua/sites/default/files/venice_grand_chanel_22.jpg', isSpecial: true },
-    { id: 2, name: 'Гірський трекінг', destination: 'Альпи', startDate: '2026-01-10', endDate: '2026-01-20', price: 1500, description: 'Активний відпочинок серед гір.', imageUrl: 'https://gra.travel/media/images/first-alps.width-1920.jpg', isSpecial: false }
-  ];
+export class ItemsList implements OnInit {
 
+  trips: Trip[] = [];
   searchText: string = '';
 
-  // Метод, який обробляє подію з дочірнього компонента
+  constructor(private tripDataService: TripDataService) {}
+
+  ngOnInit(): void {
+    this.trips = this.tripDataService.getItems();
+  }
+
   onTripSelected(trip: Trip) {
-    console.log('Обрана подорож:', trip);
     alert(`Ви обрали подорож: ${trip.name} до ${trip.destination}`);
   }
 
